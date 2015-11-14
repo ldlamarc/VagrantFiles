cee500d7980f9b43fc276f3ddc1a678e99afa4bc
ldlamarc <laurens.dlm@gmail.com>
2015-11-14 15:39:26 +0100

    Fix scripts

--- ruby-mariadb/vagrant_scripts/ruby.sh
+++ ruby-mariadb/vagrant_scripts/ruby.sh
@@ -1,2 +1,17 @@
-echo Ruby install via RVM
-rvm install 2.2.1 >/dev/null
+ #!/usr/bin/env bash
+ # https://rvm.io/integration/vagrant#using-rvm-with-vagrant
+
+ source $HOME/.rvm/scripts/rvm
+
+ echo "Installing ruby and basic gems using RVM"
+
+ rvm use --default --install $1
+
+ shift
+
+ if (( $# ))
+ then gem install $@
+ fi
+
+ rvm cleanup all
+
--- ruby-mariadb/vagrant_scripts/rvm.sh
+++ ruby-mariadb/vagrant_scripts/rvm.sh
@@ -1,6 +1,6 @@
+#!/usr/bin/env bash
+# https://rvm.io/integration/vagrant#using-rvm-with-vagrant
 echo "Installing RVM"
-if ! type rvm >/dev/null 2>&1; then
-  curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-  curl -L https://get.rvm.io | bash -s stable
-  source /home/vagrant/.rvm/scripts/rvm
-fi
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s $1
